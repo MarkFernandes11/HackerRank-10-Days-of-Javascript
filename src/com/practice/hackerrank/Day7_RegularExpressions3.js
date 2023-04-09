51242bf58927c4c25defa25ec01ce349d7342764
MarkFernandes11 <markfernandes66@gmail.com>
2023-04-09 16:31:56 +0530

    Added day7 code for regular expressions in javascript

---
+++ src/com/practice/hackerrank/Day7_RegularExpressions3.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function regexVar() {
+    /*
+     * It must match ALL occurrences of numbers in a string.
+     */
+    const re = new RegExp(/[0-9]+/g); // using g to perform a global match rather than stopping after the first match
+    
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    const r = s.match(re);
+    
+    for (const e of r) {
+        console.log(e);
+    }
+}
